423474295e27819dd5ee781277db88492e1df10d
MohamedAhmed <48156488+mohamed244134@users.noreply.github.com>
2021-11-18 18:27:29 +0200

    feat: final app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Button from './Button'
+const Header = ({title , onAdd , showAdd}) => {
+  return (
+      <header className='header'>
+          <h1>{title}</h1>
+          <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}></Button>
+      </header>
+  )
+}
+
+export default Header
